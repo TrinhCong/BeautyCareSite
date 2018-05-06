11a72a6e27a0b57b09a658f02864cce407fde614
TrinhCong <tvcprogrammer@gmail.com>
2018-05-06 19:13:52 +0700

    Add feedback form

--- js/form.js
+++ js/form.js
@@ -6,56 +6,70 @@
            $("#show").click(function () {
                $(".main-box-atm").show();
            });
+
+           $("#form").submit(function(){
+            var name=$("#name").val();
+            if(name==""){
+             $("#erroName").html(" * Enter name");
+            }else{
+             $("#erroName").html(" *");
+            }
+            //telephone
+            var telephone = $("#telephone").val();
+            if (telephone == "") {
+                $("#erroTelephone").html(" * Enter telephone");
+            } else {
+                $("#erroTelephone").html(" *");
+            }
+            //email
+            var email = $("#email").val();
+            if (email == "") {
+                $("#erroEmail").html(" * Enter Email");
+            } else {
+                $("#erroEmail").html(" *");
+            }
+            //address
+            var address = $("#address").val();
+            if (address == "") {
+                $("#erroAddress").html(" * Enter address");
+            } else {
+                $("#erroAddress").html(" *");
+            }
+            //code
+            var code = $("#code").val();
+            if (code == "") {
+                $("#erroCode").html(" * Enter code");
+            } else {
+                $("#erroCode").html(" *");
+            }
+            //card
+             var card = $("#card").val();
+            if (card == "") {
+                $("#erroCard").html(" * Enter name");
+            } else {
+                $("#erroCard").html(" *");
+            }
+            //number
+            var number = $("#number").val();
+            if (number == "") {
+                $("#erroNumber").html(" * Enter card number");
+            } else {
+                $("#erroNumber").html(" *");
+            }
+            //comment
+            var comment = $("#comment").val();
+            if (comment == "") {
+                $("#erroComment").html(" * Write your comment");
+            } else {
+                $("#erroComment").html(" *");
+            }
+            var show=$("#show");
+            var hide=$("#hide");
+            if(show.is(":checked")==false&&hide.is(":checked")==false)
+             $("#erroPay").html(" * Choose a purchase Method");
+            else
+                $("#erroPay").html(" * ");
+
+            return false;
+           });
        });
-       function getInfo() {
-           //name
-           var name=document.getElementById("name").value;
-           if(name==""){
-            document.getElementById("erroName").innerHTML="Enter name"
-           }else{
-            document.getElementById("erroName").innerHTML = ""
-           }
-           //telephone
-           var telephone = document.getElementById("telephone").value;
-           if (telephone == "") {
-               document.getElementById("erroTelephone").innerHTML = "Enter telephone"
-           } else {
-               document.getElementById("erroTelephone").innerHTML = ""
-           }
-           //email
-           var email = document.getElementById("email").value;
-           if (email == "") {
-               document.getElementById("erroEmail").innerHTML = "Enter Email"
-           } else {
-               document.getElementById("erroEmail").innerHTML = ""
-           }
-           //address
-           var address = document.getElementById("address").value;
-           if (address == "") {
-               document.getElementById("erroAddress").innerHTML = "Enter address"
-           } else {
-               document.getElementById("erroAddress").innerHTML = ""
-           }
-           //code
-           var code = document.getElementById("code").value;
-           if (code == "") {
-               document.getElementById("erroCode").innerHTML = "Enter code"
-           } else {
-               document.getElementById("erroCode").innerHTML = ""
-           }
-           //card
-            var card = document.getElementById("card").value;
-           if (card == "") {
-               document.getElementById("erroCard").innerHTML = "Enter name"
-           } else {
-               document.getElementById("erroCard").innerHTML = ""
-           }
-           //number
-           var number = document.getElementById("number").value;
-           if (number == "") {
-               document.getElementById("erroNumber").innerHTML = "Enter card number"
-           } else {
-               document.getElementById("erroNumber").innerHTML = ""
-           }
-           
-       }
