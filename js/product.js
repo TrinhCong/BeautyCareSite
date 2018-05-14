c12544ae48ca2dbd9f34a766d9b2d3c1bb840474
Trịnh Công <tvcprogrammer@gmail.com>
2018-05-14 21:59:45 +0700

    upload all page

---
+++ js/product.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function ($) {
+	//quantity-btn
+	$("#plus").click(function () {
+		var qty = parseInt($("#Quantity").val());
+		qty = qty + 1;
+		$("#Quantity").val(qty);
+		var price=parseFloat($("#price").text());
+		var total=price*qty;
+		$("#total").html(total+".00");
+	});
+	$("#minus").click(function () {
+		var qty = parseInt($("#Quantity").val());
+		qty = qty - 1;
+		if(qty>=0)
+		{
+			$("#Quantity").val(qty);
+			var price=parseFloat($("#price").text());
+			var total=price*qty;
+			$("#total").html(total+".00");
+		}
+			
+	});
+
+	//slider
+	var jssor_1_options = {
+		$AutoPlay: 1,
+		$AutoPlaySteps: 5,
+		$SlideDuration: 500,
+		$SlideWidth: 200,
+		$SlideSpacing: 3,
+		$ArrowNavigatorOptions: {
+			$Class: $JssorArrowNavigator$,
+			$Steps: 1
+		},
+		$BulletNavigatorOptions: {
+			$Class: $JssorBulletNavigator$
+		}
+	};
+
+	var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+	/*#region responsive code begin*/
+
+	var MAX_WIDTH = 980;
+
+	function ScaleSlider() {
+		var containerElement = jssor_1_slider.$Elmt.parentNode;
+		var containerWidth = containerElement.clientWidth;
+
+		if (containerWidth) {
+
+			var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+			jssor_1_slider.$ScaleWidth(expectedWidth);
+		}
+		else {
+			window.setTimeout(ScaleSlider, 30);
+		}
+	}
+
+	ScaleSlider();
+
+	$(window).bind("load", ScaleSlider);
+	$(window).bind("resize", ScaleSlider);
+	$(window).bind("orientationchange", ScaleSlider);
+	/*#endregion responsive code end*/
+
+	
+});
