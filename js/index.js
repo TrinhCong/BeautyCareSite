76cde0ff2518e93a2651d2ca9bc7df4d4b6a7492
Trịnh Công <tvcprogrammer@gmail.com>
2018-05-20 18:20:20 +0700

    Add services Page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿new WOW().init();
+$(document).ready(function () {
+    $("a[href='#top']").click(function () {
+        $("html, body").animate({ scrollTop: 400 }, "slow");
+        return false;
+    });
+    window.onscroll = function () { scrollFunction() };
+    function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            if ($(".page-mark").hasClass('d-none'))
+                $(".page-mark").removeClass('d-none');
+        } else {
+            $(".page-mark").addClass('d-none');
+        }
+    }
+});
